5b9cbf9b7da5d49f14a2e1b504b56399c53d760b
Geometry Tryhard <memelite69@gmail.com>
2026-02-12 20:46:00 -0500

    ashpxl

--- js/games.js
+++ js/games.js
@@ -92,12 +92,12 @@ function loadGames(list, divid) {
 
   const frag = document.createDocumentFragment();
   sorted.forEach(g => {
-    const div = document.createElement("a");
-    div.className = `game ${g.title} unblocked AshPXL`;
+    const div = document.createElement("div");
+    div.className = "game";
 
     const img = document.createElement("img");
     img.src = g.image;
-    img.alt = `${g.title} unblocked AshPXL` || "game";
+    img.alt = g.title || "game";
     img.loading = "lazy";
     div.appendChild(img);
 
@@ -113,7 +113,6 @@ function loadGames(list, divid) {
         .replace(/\s+/g, "-")
         .replace(/-+/g, "-")
     );
-    div.href=`/g/${safeTitle}`;
     div.onclick = () => {
       var iframe = document.getElementById('gameiframe');
       if(iframe){
